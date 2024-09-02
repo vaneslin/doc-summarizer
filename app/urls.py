180b2f73612a582c2096cb00158630cdb1a85e23
vaneslin <vantestins@gmail.com>
2024-09-02 23:22:02 +0100

    added minor comments to views & url pages

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+"""
+    This is a router that defines URL patterns for the app. Path has three arguments:
+    1. A regular expression: This is a string that matches the URL pattern. In this case, the empty string represents the base URL.
+    2. A view function: This is the function that will be called when the URL pattern is matched. 
+    3. A name: This is the name of the URL pattern. This is used to refer to the URL pattern in the code.
+"""
 urlpatterns = [
     path('', views.index, name='index'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse as HTTPResponse
 
+# Create your views here.
+
 
 def index(request):
+    # This view function takes an HTTP request and returns an HTTP response.
+    # In this case, it renders the index.html template as a dynamic HTML page (server side rendering).
+
+    # When using React, this function would be different.
+    # It would return a JSON response with the backend data i.e. database data to be rendered by React, because React would handle the frontend rendering.
     return render(request, 'app/index.html')
-# Create your views here.
